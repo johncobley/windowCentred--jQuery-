989362ed2b77168a1d105cf2f8b29f51223e6554
johncobley <john.cobley@comtek.co.uk>
2015-05-28 17:58:17 +0100

    Initial Commit
    Added Plugin

---
+++ windowCentred.js
@@ -0,0 +1,9 @@
+﻿jQuery.fn.windowCentred = function () {
+    var container = window;
+    if ($(container).height() == 0)
+        container = document;
+    this.css("position", "absolute");
+    this.css("top", Math.max(0, (($(container).height() - this.outerHeight()) / 2) + $(container).scrollTop()) + "px");
+    this.css("left", Math.max(0, (($(container).width() - this.outerWidth()) / 2) + $(container).scrollLeft()) + "px");
+    return this;
+}
